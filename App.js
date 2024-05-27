207cc2388cfd1b16fe2638790b990f7a336b2db6
amfafa <amfafa@amfafas-MacBook-Pro.local>
2024-05-27 16:05:27 +0000

    bolded name text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is <Text>Jochebed Fafa</Text></Text>
+      <Text>My name is <Text style={{fontWeight:'bold'}}>Jochebed Fafa</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
